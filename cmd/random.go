75e9442c35be27277397cf47c3dd19652950b879
KennethSC <kennethsantacruz12@gmail.com>
2021-08-21 00:06:28 -0700

    Fixed out of bounds error when selecting random joke from search term genereated jokes list

--- cmd/random.go
+++ cmd/random.go
@@ -78,11 +78,13 @@ func getRandomJokeWithTerm(searchTerm string) {
 	rand.Seed(time.Now().UnixNano())
 
 	totalJokes, jokes := requestJokeWithTerm(searchTerm)
+	min := 0
+	max := totalJokes - 1
 
 	if totalJokes <= 0 {
 		fmt.Println("No jokes could be found with the search term")
 	} else {
-		randomIdx := rand.Intn(len(jokes)-0) + len(jokes)
+		randomIdx := min + rand.Intn(max-min)
 		fmt.Println(jokes[randomIdx].Joke)
 	}
 }
